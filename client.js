35ee07962dd9b8b6072e8507d80c5a9a33efbc1e
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-10-01 15:44:51 -0500

    added object of employee to function

--- client.js
+++ client.js
@@ -5,4 +5,24 @@ $(handleStart);
 function handleStart(){
     console.log('Jquery ready for action');
     
+//click listeners
+$(`#addEmployee`).on(`click`, addEmployee);
+
+
+}
+
+function addEmployee(){
+    console.log('click'); //check click
+    
+    const firstName = $(`#firstName`).val();
+    console.log(firstName); //check input
+    const employee ={
+        firstName:$(`#firstName`).val(),
+        lastName:$(`#lastName`).val(),
+        id:$(`#id`).val(),
+        title:$(`#title`).val(),
+        annualSalary:$(`#annualSalary`).val(),
+    }
+    console.log(employee);
+    
 }
