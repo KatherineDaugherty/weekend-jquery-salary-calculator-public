d5a1bd6c8762739ced87fe4becb544a668215a2b
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-10-01 16:02:25 -0500

    append to DOM with button click

--- client.js
+++ client.js
@@ -8,8 +8,14 @@ function handleStart(){
 //click listeners
 $(`#addEmployee`).on(`click`, addEmployee);
 
+// $(`#target`).on(`click`,`.`)
 
 }
+function deleteEmployee(){
+    console.log('DELETE');
+    
+}
+
 
 function addEmployee(){
     console.log('click'); //check click
@@ -25,4 +31,15 @@ function addEmployee(){
     }
     console.log(employee);
     
+const row = $(`
+<tr>
+    <td>${employee.firstName}</td>
+    <td>${employee.lastName}</td>
+    <td>${employee.id}</td>
+    <td>${employee.title}</td>
+    <td>${employee.annualSalary}</td>
+</tr>
+`);
+
+    $(`#target`).append(row);
 }
